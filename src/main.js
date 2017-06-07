2d3b58e667415085d82b5de8fa4d01119629bdfe
pjchender <pjchender@gmail.com>
2017-06-07 15:55:32 +0800

    the simplest SPA which can run on Express

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 import router from './router'
+import storeOption from './vuex/store'
+
+/**
+ * Use Vuex
+ * 建立一個 store 物件用來管理 app 的資料
+ */
+Vue.use(Vuex)
+const store = new Vuex.Store(storeOption)
 
 Vue.config.productionTip = false
 
@@ -10,6 +19,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Hello from '@/views/Hello.vue'
+import NotFound from '@/views/notfound.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '*',
+      name: 'notFound',
+      component: NotFound
     }
   ]
 })
