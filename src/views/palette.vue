11018ac8a6bfa8a2cbdc40c0b8ac91e96ed736dd
pjchender <pjchender@gmail.com>
2017-06-08 14:54:40 +0800

    Sort color by hue

--- src/views/palette.vue
+++ src/views/palette.vue
@@ -4,8 +4,9 @@
     <hr>
     <div class="enter-color">
       <div class="color-to-pick">
-        <input type="color" v-model="pickColor"/>
-        <input type="text" placeholder="#FFFFFF" v-model="pickColor" @keyup.enter="addColor"/>
+        <input type="color" v-model="pickColor.hexColor"/>
+        <input type="text" placeholder="#FFFFFF" v-model="pickColor.hexColor" @keyup.enter="addColor"/>
+        <input type="text" placeholder="comment" v-model="pickColor.comment">
       </div>
       <button type="button"
         :class="['btn', {'btn-outline-secondary': !verifyColor}, {'btn-outline-primary': verifyColor}]" 
@@ -15,70 +16,130 @@
     <div class="color-palette" v-show="colorPalette.length">
       <ul>
         <li class="color-block" 
-          v-for="color in colorPalette" 
-          v-bind:style="{'background-color': color}" 
-          @click.prevent.stop="showColor">
-            <span class="delete-color" @click.prevent.stop="deleteColor(color)">X</span>
-          </li>
+          v-for="item in colorPalette" 
+          :style="{'background-color': item.hexColor}" 
+          @click.prevent.stop="showColor(item.hexColor, item.comment)">
+            <span 
+            class="delete-color"
+            @click.prevent.stop="deleteColor(item.hexColor)">X</span>
+        </li>
       </ul>
     </div>
   </div>
 </template>
-
+ 
 <script>
+
+import hexToHsl from 'hex-to-hsl'
+
+let colorPalette = [
+    {
+        comment: 'green',
+        hexColor: '#41CEC0' 
+    },
+    {
+        comment: 'vue',
+        hexColor: '#41b883' 
+    },
+     {
+        comment: 'node',
+        hexColor: '#43853d' 
+    },
+    {
+        comment: 'blue',
+        hexColor: '#35495e' 
+    },
+    {
+        comment: 'dark-blue',
+        hexColor: '#3d4f5d' 
+    },
+    {
+        comment: 'purple',
+        hexColor: '#A53860' 
+    },
+    {
+        comment: 'heroku',
+        hexColor: '#79589F' 
+    },
+    {
+        comment: 'red',
+        hexColor: '#C63647' 
+    },
+    {
+        comment: 'npm',
+        hexColor: '#C12127' 
+    },{
+        comment: 'yellow',
+        hexColor: '#FFDF2C' 
+    },
+    {
+        comment: 'wesbos',
+        hexColor: '#ffc600' 
+    },
+    {
+        comment: 'orange',
+        hexColor: '#E96900' 
+    },
+    {
+        comment: 'black',
+        hexColor: '#282c34'
+    }
+]
+
+
 export default {
   name: 'palette',
   data () {
     return {
-      pickColor: "",
-      colorPalette: []
+      pickColor: {
+        hexColor: '',
+        comment: ''
+      },
+      colorPalette: null
     }
   },
   computed: {
     verifyColor () {
-      return /^#[0-9A-F]{6}$/i.test(this.pickColor)
+      return /^#[0-9A-F]{6}$/i.test(this.pickColor.hexColor)
     }
   },
   methods: {
-    rgbToHex(rgbColor) {
-      rgbColor = rgbColor.replace(/[^\d,]/g, "").split(",")
-      function toHex(n) {
-        n = parseInt(n, 10)
-        if (isNaN(n)) return "00"
-        n = Math.max(0, Math.min(n, 255))
-        return (
-          "0123456789ABCDEF".charAt((n - n % 16) / 16) +
-          "0123456789ABCDEF".charAt(n % 16)
-        )
-      }
-      return (
-        "#" +
-        toHex(rgbColor[0]) +
-        toHex(rgbColor[1]) +
-        toHex(rgbColor[2])
-      )
-    },
     addColor() {
       if (!this.verifyColor) {
         this.$store.commit('emit/Alert', "並非有效的色碼")
-      } else if (this.checkSameColor(this.pickColor)) {
+      } else if (this.checkSameColor(this.pickColor.hexColor)) {
         this.$store.commit('emit/Alert', "此顏色已設定過")
       } else {
-        this.colorPalette.splice(0, 0, this.pickColor.toUpperCase())
+        this.colorPalette.splice(0, 0, {
+          hexColor: this.pickColor.hexColor.toUpperCase(),
+          name: null,
+          comment: null,
+          hue: hexToHsl(this.pickColor.hexColor)[0]
+        })
       }
     },
-    showColor(e) {
-      let rgbColor = window.getComputedStyle(e.target).getPropertyValue("background-color");
-      this.pickColor = this.rgbToHex(rgbColor);
+    showColor(hexColor, comment) {
+      this.pickColor.hexColor = hexColor
+      this.pickColor.comment = comment
     },
     checkSameColor(hexColor) {
-      let findColor = this.colorPalette.some(item => item === hexColor);
+      let findColor = this.colorPalette.some(item => item.hexColor === hexColor);
       return findColor;
     },
-    deleteColor(color){
-      let deleteColorIndex = this.colorPalette.findIndex(item => item === color)
+    deleteColor(hexColor){
+      let deleteColorIndex = this.colorPalette.findIndex(item => item.hexColor.toUpperCase() === hexColor.toUpperCase())
       this.colorPalette.splice(deleteColorIndex, 1)
     }
+  },
+  created () {
+    this.colorPalette = colorPalette.map(item => {
+      return Object.assign({}, item, {
+        hue: hexToHsl(item.hexColor)[0]
+      })
+    })
+
+    // sort the color by hue
+    this.colorPalette.sort((a, b) => (a.hue - b.hue) > 0 ? 1 : -1 )
   }
 }
 </script>
