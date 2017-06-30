4dcdf1532b90a9721fdc87468dad5c9a729de11b
pjchender <pjchender@gmail.com>
2017-06-30 11:04:40 +0800

    Add Search function. Fixed getPalette Issued

--- src/views/palette.vue
+++ src/views/palette.vue
@@ -10,7 +10,7 @@
           @click.prevent.stop="exportPalette"
         >{{ words.view.export }}</button>
         <button type="button"
-          :class="['btn', 'col-md-2',{'btn-outline-secondary': !localNewerThenSever},{'btn-outline-primary': localNewerThenSever}]"
+          :class="['btn', 'col-md-2',{'btn-outline-secondary': equalLocalAndServer},{'btn-outline-primary': !equalLocalAndServer}]"
           @click.prevent.stop="savePaletteToServer"
         >{{ words.view.save }}</button>
       </div>
@@ -18,14 +18,14 @@
       <div class="color-to-pick row justify-content-md-center align-items-center">
         <div class="col-md-6">
           <input type="color" v-model="pickColor.hexColor">
-          <input type="text" placeholder="#FFFFFF" 
+          <input type="text" placeholder="#FFFFFF"
+            id="input-hex-color"
             v-model="pickColor.hexColor"
             @keyup.enter="enterHandler"
             >
           <input type="text" placeholder="variable name" 
             v-model="pickColor.name"
             @keyup.enter="enterHandler"
-            @input="findColor"
             >
         </div>
         <div class="col-md-1">
@@ -44,18 +44,31 @@
         </div>
       </div>
     </div>
+
+    
     <div class="color-palette container" v-show="colorPalette.length">
-      <ul>
-        <li class="color-block" 
-          v-for="item in colorPalette"
-          :key="item.hexColor"
-          :style="{'background-color': item.hexColor}" 
-          @click.prevent.stop="showColor(item.hexColor, item.name)">
-            <span 
-            class="delete-color"
-            @click.prevent.stop="deleteColor(item.hexColor)">X</span>
-        </li>
-      </ul>
+      <div class="row">
+        <div class="col-12">
+          <input type="text" class="search-input" placeholder="Search by Variable Name" 
+            v-model="searchColor"
+          >
+        </div>
+      </div>
+      <div class="row justify-content-start">
+        
+          <ul class="col-12">
+            <li class="color-block" 
+              v-for="item in visibleColorPalette"
+              :key="item.hexColor"
+              :style="{'background-color': item.hexColor}" 
+              @click.prevent.stop="showColor(item.hexColor, item.name)">
+                <span 
+                class="delete-color"
+                @click.prevent.stop="deleteColor(item.hexColor)">X</span>
+            </li>
+          </ul>
+        
+      </div>
     </div>
     <textarea hidden id="contentToExport" v-model="contentToExport"></textarea>
   </div>
@@ -64,9 +77,8 @@
 <script>
 import hexToHsl from 'hex-to-hsl'
 import request from 'superagent'
-let endpoint = 'https://simple-webapps.herokuapp.com/v1.0'
 
-
+let endpoint = process.env.NODE_ENV ? 'https://simple-webapps.herokuapp.com/v1.0' : 'http://localhost:3000/v1.0'
 
 export default {
   name: 'palette',
@@ -76,10 +88,12 @@ export default {
         hexColor: '',
         name: ''
       },
+      searchColor: '',
       colorPalette: [],
       colorPaletteUpdatedAt: '',
       colorPaletteOnCloud: [],
       colorPaletteOnCloudUpdatedAt: '',
+      localStorageIsExist: false,
       words: {
         view: {
           export: 'Export',
@@ -102,10 +116,23 @@ export default {
     }
   },
   computed: {
+    visibleColorPalette () {
+      return this.colorPalette.filter(color => {
+        let regex = new RegExp(this.searchColor, 'gi')
+        return color.name.match(regex)
+      })
+    },
     credential () {
+      let credentialState = this.$store.state.credentialModule
+      // #1-1: 向 server 要資料
+      if (credentialState.authorized && credentialState.accessToken) {
+        // #2-1 facebook 已登入
+        this.getPaletteFromServer(credentialState.accessToken)
+      }
+      // #2-2 facebook 未登入
       return {
-        accessToken: this.$store.state.credentialModule.accessToken || null,
-        authorized: this.$store.state.credentialModule.authorized || false
+        accessToken: credentialState.accessToken || null,
+        authorized: credentialState.authorized || false
       }
     },
     verifyColor () {
@@ -126,10 +153,10 @@ export default {
       }
       return false
     },
-    localNewerThenSever () {
+    equalLocalAndServer () {
       let local = new Date(this.colorPaletteUpdatedAt).getTime()
       let server = new Date(this.colorPaletteOnCloudUpdatedAt).getTime()
-      return (local > server) ? true : false
+      return (local === server) ? true : false
     }
   },
   methods: {
@@ -144,15 +171,14 @@ export default {
           hue: hexToHsl(this.pickColor.hexColor)[0]
         })
         this.colorPalette.splice(0, 0, pickColor)
+        if (!this.localStorageIsExist) {
+          // #4-4 facebook 未登入 > localStorage 未建立 > 使用者輸入資料
+          this.savePaletteToLocalStorage(this.colorPalette)
+        }
         this.pickColor.hexColor = ''
         this.pickColor.name = ''
+        document.querySelector('#input-hex-color').focus()
       }
-    },
-    findColor () {
-      this.colorPalette = this.colorPalette.filter(color => {
-        let regex = new RegExp(this.pickColor.name, 'gi')
-        return color.name.match(regex)
-      })
     },
     updateColor () {
       let colorIndex = this.colorPalette.findIndex(item => item.hexColor.toUpperCase() === this.pickColor.hexColor.toUpperCase());
@@ -167,6 +193,7 @@ export default {
       this.colorPalette.splice(colorIndex, 1, pickColor)
       this.pickColor.hexColor = ''
       this.pickColor.name = ''
+      document.querySelector('#input-hex-color').focus()
     },
     showColor(hexColor, name) {
       this.pickColor.hexColor = hexColor
@@ -206,8 +233,26 @@ export default {
      * Interact with LocalStorage
     **/
     getPaletteFromLocalStorage () {
-      this.colorPalette = JSON.parse(localStorage.getItem('colorPalette')) || []
-      this.colorPaletteUpdatedAt = localStorage.getItem('paletteUpdateAt') || ''
+      if (!localStorage.getItem('colorPalette')) {
+        // #3-2: localStorage 尚未建立
+        // #3-4: localStorage 尚未建立
+        this.localStorageIsExist = false
+      } else {
+        // #3-1: facebook 已登入 > localStorage 曾建立
+        // #3-3: facebook 未登入 > localStorage 曾建立
+        // #4-1: facebook 已登入 > localStorage 曾建立 > 存取 localStorage 的資料
+        // #4-3: facebook 未登入 > localStorage 曾建立 > 存取 localStorage 的資料
+        this.colorPalette = JSON.parse(localStorage.getItem('colorPalette'))
+        this.colorPaletteUpdatedAt = localStorage.getItem('paletteUpdateAt')
+        this.localStorageIsExist = true
+      }
+    },
+    savePaletteToLocalStorage (value = []) {
+      localStorage.setItem('colorPalette', JSON.stringify(value))
+      localStorage.setItem('paletteUpdateAt', new Date().toISOString())
+      this.colorPaletteUpdatedAt = new Date().toISOString()
+      this.localStorageIsExist = true
+      this.$store.commit('emit/Flash', this.words.controller.autoUpdatedSuccessLocal)
     },
     /**
      * Interact with Server
@@ -225,28 +270,32 @@ export default {
         vm.colorPaletteOnCloudUpdatedAt = response.newPalette.updated_at
       })
     },
-    getPaletteFromServer(){
+    getPaletteFromServer(accessToken){
       let vm = this
       request.get(endpoint + '/palette/')
       .type('application/json')
-      .set({Authorization: vm.credential.accessToken})
+      .set({Authorization: accessToken})
       .end((err, res) => {
         if (err) {
           this.$store.commit('emit/Alert', err)
         }
         let response = JSON.parse(res.text)
         if (response.status === 400) {
-          // Palette 尚未建立
+          // Server 上的 Palette 尚未建立
           vm.createPaletteToServer()
         } else if (response.status === 200) {
           vm.colorPaletteOnCloud = response.colors
           vm.colorPaletteOnCloudUpdatedAt = response.updated_at
-
-          if (!localNewerThenSever) {
-            // 如果 server 的資料比較新（或 local 不存在）
+          
+          if (vm.localStorageIsExist) {
+            // #4-1 存取 localStorage 的資料
+            // vm.savePaletteToServer()
+          } else {
+            // #4-2 facebook以登入 > localStorage 尚未建立 > 存取 Server 的資料
             vm.colorPalette = response.colors || []
             vm.colorPaletteUpdatedAt = response.updated_at || ''
           }
+
         } else {
           this.$store.commit('emit/Alert', "Error in getPaletteFromServer in Palette.vue")
         }
@@ -277,27 +326,34 @@ export default {
     }
   },
   watch: {
-    colorPalette (value) {
-      // 當 colorPalette 有改變的時候自動存到 localStorage
-      localStorage.setItem('colorPalette', JSON.stringify(value))
-      localStorage.setItem('paletteUpdateAt', new Date().toISOString())
-      this.colorPaletteUpdatedAt = new Date().toISOString()
-      this.$store.commit('emit/Flash', this.words.controller.autoUpdatedSuccessLocal)
+    colorPalette: {
+      handler: function (val, oldVal) {
+        if (this.localStorageIsExist) {
+          this.savePaletteToLocalStorage(val)
+        }
+      },
+      deep: true,
+      immediate: true
     },
-    credential (value) {
-      // 把 credential 放在 watch 中是要促發 computed 
-      if (value.authorized && value.accessToken) {
-        this.getPaletteFromServer()
-      }
+    credential: {
+      // 把 credential 放在 watch 是希望能夠促使 computed credential 的資料更新
+      handler: function(val, oldVal){
+        return
+      },
+      deep: true,        // 監聽物件內部值的變化
+      immediate: true    // 立即以 someObject 的觸發值執行 callback
     }
   },
   created () {
+    // #1-2 向 localStorage 要資料
     this.getPaletteFromLocalStorage()
+    
     this.colorPalette = this.colorPalette.map(item => {
       return Object.assign({}, item, {
         hue: hexToHsl(item.hexColor)[0]
       })
     })
+    
     // sort the color by hue
     this.colorPalette.sort((a, b) => (a.hue - b.hue) > 0 ? 1 : -1 )
   }
@@ -346,10 +402,6 @@ $disable: #CCC;
     }
 }
 
-
-
-
-
 .export-or-save{
   margin-top: 30px;
   margin-bottom: 30px;
@@ -359,20 +411,34 @@ $disable: #CCC;
     min-width: 480px;
     border: 1px solid #CCC;
     margin-top: 16px;
-    padding: 10px;
+    padding: 10px 30px;
     ul {
-        margin: 0;
-        padding: 0;
-        list-style: none;
-        font-size: 0px;
+      list-style: none;
+      font-size: 0px;
+      display: flex;
     }
+
+    .search-input {
+      width: 100%;
+      padding: 0.5rem 1rem;
+      font-size: 1rem;
+      border-radius: 0.25rem;
+      line-height: 1.25rem;
+      outline: 0;
+      border: 1px solid #CCC;
+      margin: 20px auto;
+
+      &::placeholder {
+        color: $disable;
+      }
+    }
+
     .color-block {
         cursor: pointer;
         width: 70px;
         height: 70px;
         margin: 7px;
         background-color: $red;
-        display: inline-block;
         position: relative;
         .delete-color {
             position: absolute;
