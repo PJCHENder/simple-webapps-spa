e2433363c29215e68fff23bcc0ee55efdcd73d9b
pjchender <pjchender@gmail.com>
2017-09-18 14:09:49 +0800

    Modify mindful timer stylesheet

--- src/views/mindful_timer.vue
+++ src/views/mindful_timer.vue
@@ -2,68 +2,60 @@
   <div class="wrapper">
 
     <div class="col-3 alert alert-success text-center" role="alert">
-        Saved Successfully
+      Saved Successfully
     </div>
 
     <div class="container-fluid">
-
-        <div class="timer-panel">
-
-          <transition name="fade">
-            <div v-if="!restTimeString" class="set-task-panel">
-              <div class="form-group row no-gutters">
-                  <label for="set-task" class="col-4 col-form-label">寫下任務</label>
-                  <div class="col-8">
-                      <input class="form-control" type="text" id="set-task" placeholder="寫 Code" v-model="setTask">
-                  </div>
-              </div>
-
-              <div class="set_time form-group row no-gutters">
-                  <label for="set_time" class="col-4 col-form-label">設定分鐘</label>
-                  <div class="col-8">
-                      <input class="form-control" type="number" id="set_time" placeholder="20" v-model="setTaskTime">
-                  </div>
+      <div class="timer-panel">
+        <transition name="fade">
+          <div v-if="!restTimeString" class="set-task-panel">
+            <div class="form-group row no-gutters">
+              <label for="set-task" class="col-4 col-form-label">寫下任務</label>
+              <div class="col-8">
+                <input class="form-control" type="text" id="set-task" placeholder="寫 Code" v-model="setTask">
               </div>
             </div>
 
-            <div v-else class="show-task-panel card text-center">
-              <div class="card-header">
-                  目前任務
-              </div>
-              <div class="card-block">
-                  <h4 class="card-title">{{ currentTask }}</h4>
-              </div>
-              <div id="show" class="card-footer text-muted">
-                {{ restTimeString }}
+            <div class="set_time form-group row no-gutters">
+              <label for="set_time" class="col-4 col-form-label">設定分鐘</label>
+              <div class="col-8">
+                <input class="form-control" type="number" id="set_time" placeholder="20" v-model="setTaskTime">
               </div>
             </div>
-          </transition>
+          </div>
 
-            <div class="row no-gutters">
-                <button type="button" class="btn btn-outline-primary btn-block" id="clocking"
-                  @click.prevent.stop="startHandler"
-
-                  v-show="!restTimeString"
-                  >Start</button>
-                <button type="button" class="btn btn-outline-primary btn-block" id="reset"
-                  @click.prevent.stop="resetTimer"
-                  v-show="restTimeString"
-                  >Reset</button>
+          <div v-else class="show-task-panel card text-center">
+            <div class="card-header">
+              目前任務
             </div>
-
-            <div class="row card no-gutters card-outline-secondary mb-3 text-center" v-show="logger.length > 0">
-                <ul class="card-block" id="log">
-                  <li v-for="item in logger" :key="item.time">{{`${item.time} => ${item.task}`}}</li>
-                </ul>
+            <div class="card-block">
+              <h4 class="card-title">{{ currentTask }}</h4>
             </div>
+            <div id="show" class="card-footer text-muted">
+              {{ restTimeString }}
+            </div>
+          </div>
+        </transition>
 
+        <div class="row no-gutters">
+          <button type="button" class="btn btn-outline-primary btn-block" id="clocking" @click.prevent.stop="startHandler" v-show="!restTimeString">Start</button>
+          <button type="button" class="btn btn-outline-primary btn-block" id="reset" @click.prevent.stop="resetTimer" v-show="restTimeString">Reset</button>
         </div>
 
-        <div class="note">
-            <div v-show="isShowMarkDown" v-html="compiledMarkdown" class="form-control" id="preview" @click.stop.prevent="toggleMarkDown(isShowMarkDown, $event)"></div>
-            <textarea v-show="!isShowMarkDown" class="form-control" id="note" v-model="noteContent" @blur.stop.prevent="toggleMarkDown(isShowMarkDown, $event)"></textarea>
-            <p class="copyright"> <a href="https://pjchender.blogspot.com" target="_blank">PJCHENder</a> </p>
+        <div class="row card no-gutters card-outline-secondary mb-3 text-center" v-show="logger.length > 0">
+          <ul class="card-block" id="log">
+            <li v-for="item in logger" :key="item.time">{{`${item.time} => ${item.task}`}}</li>
+          </ul>
         </div>
+      </div>
+
+      <div class="note">
+        <div v-show="isShowMarkDown" v-html="compiledMarkdown" class="form-control" id="preview" @click.stop.prevent="toggleMarkDown(isShowMarkDown, $event)"></div>
+        <textarea v-show="!isShowMarkDown" class="form-control" id="textarea" v-model="noteContent" @blur.stop.prevent="toggleMarkDown(isShowMarkDown, $event)"></textarea>
+        <p class="copyright">
+          <a href="https://pjchender.blogspot.com" target="_blank">PJCHENder</a>
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -77,12 +69,12 @@ let endpoint = (process.env.NODE_ENV === "production") ? 'https://simple-webapps
 let UNSET_TASK = '未設定'
 
 /** * debounce function * 讓某函式在一定時間內只能觸發一次，目的是提升效能**/
-function _debounce(func, wait = 20, immediate = true) {
+function _debounce (func, wait = 20, immediate = true) {
   var timeout;
-  return function() {
+  return function () {
     var context = this,
       args = arguments;
-    var later = function() {
+    var later = function () {
       timeout = null;
       if (!immediate) func.apply(context, args);
     };
@@ -117,7 +109,7 @@ export default {
     },
     // 剩餘時間（字串）
     restTimeString () {
-      if (this.restMs < 0 || !this.restMs) {return false}
+      if (this.restMs < 0 || !this.restMs) { return false }
       let seconds = String(parseInt((this.restMs / 1000) % 60))
       let minutes = String(parseInt((this.restMs / (1000 * 60)) % 60))
       let resetTimeString = `${minutes.padStart(2, '0')}:${seconds.padStart(2, '0')}`
@@ -128,12 +120,10 @@ export default {
       return this.setTask || '尚未設定任務'
     },
     credential () {
-      console.log('credential')
       let credentialState = this.$store.state.credentialModule
       // #1-1: 向 server 要資料
       if (credentialState.authorized && credentialState.accessToken) {
         // #2-1 facebook 已登入
-        console.log('getNoteFromServer in credential')
         this.getNoteFromServer(credentialState.accessToken)
       }
       // #2-2 facebook 未登入
@@ -226,14 +216,12 @@ export default {
       if (!localStorage.getItem('noteContent')) {
         this.localStorageIsExist = false
       } else {
-        console.log('getNoteFromLocalStorage')
         this.noteContent = localStorage.getItem('noteContent')
         this.noteUpdatedAt = localStorage.getItem('noteUpdatedAt')
         this.localStorageIsExist = true
       }
     },
     saveNoteToLocalStorage: _debounce(function () {
-      console.log('saveNoteToLocalStorage')
       localStorage.setItem('noteContent', this.noteContent)
       localStorage.setItem('noteUpdatedAt', new Date().toISOString())
       this.noteUpdatedAt = new Date().toISOString()
@@ -246,74 +234,69 @@ export default {
     createNoteToServer () {
       let vm = this
       request.post(endpoint + '/note/')
-      .type('application/json')
-      .set({Authorization: vm.credential.accessToken})
-      .end((err, res) => {
-        if (err) {
-          this.$store.commit('emit/Alert', err)
-        }
-        let response = JSON.parse(res.text)
-        vm.noteOnCloudUpdatedAt = response.newNote.updated_at
-        console.log('createNoteToServer')
-      })
+        .type('application/json')
+        .set({ Authorization: vm.credential.accessToken })
+        .end((err, res) => {
+          if (err) {
+            this.$store.commit('emit/Alert', err)
+          }
+          let response = JSON.parse(res.text)
+          vm.noteOnCloudUpdatedAt = response.newNote.updated_at
+        })
     },
-    getNoteFromServer(accessToken){
+    getNoteFromServer (accessToken) {
       let vm = this
       request.get(endpoint + '/note/')
-      .type('application/json')
-      .set({Authorization: accessToken})
-      .end((err, res) => {
-        if (err) {
-          this.$store.commit('emit/Alert', err)
-        }
-        let response = JSON.parse(res.text)
-        if (response.status === 400) {
-          // Server 上的 Note 尚未建立
-          console.log('createNoteToServer')
-          vm.createNoteToServer()
-        } else if (response.status === 200) {
-          console.log('response', response)
-          vm.noteOnCloud = response.note
-          vm.noteOnCloudUpdatedAt = response.updated_at
-
-          if (vm.localStorageIsExist) {
-            // #4-1 存取 localStorage 的資料
-            // vm.saveNoteToServer()
-          } else {
-            // #4-2 facebook以登入 > localStorage 尚未建立 > 存取 Server 的資料
-            vm.noteContent = response.note || []
-            vm.noteUpdatedAt = response.updated_at || ''
+        .type('application/json')
+        .set({ Authorization: accessToken })
+        .end((err, res) => {
+          if (err) {
+            this.$store.commit('emit/Alert', err)
           }
-          console.log('getNoteFromServer')
-        } else {
-          this.$store.commit('emit/Alert', "Error in getNoteForServer in MindfulTimer.vue")
-        }
-      })
+          let response = JSON.parse(res.text)
+          if (response.status === 400) {
+            // Server 上的 Note 尚未建立
+            vm.createNoteToServer()
+          } else if (response.status === 200) {
+            vm.noteOnCloud = response.note
+            vm.noteOnCloudUpdatedAt = response.updated_at
+
+            if (vm.localStorageIsExist) {
+              // #4-1 存取 localStorage 的資料
+              // vm.saveNoteToServer()
+            } else {
+              // #4-2 facebook以登入 > localStorage 尚未建立 > 存取 Server 的資料
+              vm.noteContent = response.note || []
+              vm.noteUpdatedAt = response.updated_at || ''
+            }
+          } else {
+            this.$store.commit('emit/Alert', "Error in getNoteForServer in MindfulTimer.vue")
+          }
+        })
 
     },
     saveNoteToServer () {
-      console.log('saveNoteToServer')
       let vm = this
       request.put(endpoint + '/note')
-      .type('application/json')
-      .set({Authorization: vm.credential.accessToken})
-      .send({noteContent: vm.noteContent})
-      .end((err, res) => {
-        if (err) {
-          vm.$store.commit('emit/Alert', `Error occurred in saveNoteToServer in mindful_timer.vue(${err})`)
-        }
-        let response = JSON.parse(res.text)
+        .type('application/json')
+        .set({ Authorization: vm.credential.accessToken })
+        .send({ noteContent: vm.noteContent })
+        .end((err, res) => {
+          if (err) {
+            vm.$store.commit('emit/Alert', `Error occurred in saveNoteToServer in mindful_timer.vue(${err})`)
+          }
+          let response = JSON.parse(res.text)
 
-        if (response.status === 200) {
-          vm.noteUpdatedAt = response.updated_at
-          vm.noteOnCloudUpdatedAt = response.updated_at
-          localStorage.setItem('noteUpdatedAt', response.updated_at)
-          vm.$store.commit('emit/Flash', '成功儲存於雲端')
-        } else {
-          vm.$store.commit('emit/Alert', `Error occurred in saveNoteToServer in mindful_timer.vue(${response.message})`)
-        }
+          if (response.status === 200) {
+            vm.noteUpdatedAt = response.updated_at
+            vm.noteOnCloudUpdatedAt = response.updated_at
+            localStorage.setItem('noteUpdatedAt', response.updated_at)
+            vm.$store.commit('emit/Flash', '成功儲存於雲端')
+          } else {
+            vm.$store.commit('emit/Alert', `Error occurred in saveNoteToServer in mindful_timer.vue(${response.message})`)
+          }
 
-      })
+        })
     },
     changeFavicon () {
       var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
@@ -330,7 +313,7 @@ export default {
     },
     credential: {
       // 把 credential 放在 watch 是希望能夠促使 computed credential 的資料更新
-      handler: function(val, oldVal){
+      handler: function (val, oldVal) {
         return
       },
       deep: true,        // 監聽物件內部值的變化
@@ -345,100 +328,106 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .wrapper{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-  }
+.wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 
-  .container-fluid {
-    background-color: #E8E8E8;
-    padding: 0px 35px;
-    display: flex;
-    border-radius: 5px;
-    justify-content: space-around;
-    width: 95%;
-    height: 95%;
-  }
-  .timer-panel{
-    width: 320px;
-    margin-top: 50px;
-    margin-right: 15px;
-  }
+.container-fluid {
+  background-color: #E8E8E8;
+  padding: 0px 35px;
+  display: flex;
+  border-radius: 5px;
+  justify-content: space-around;
+  width: 95%;
+  height: 95%;
+}
 
-  .note{
-    flex-grow: 1;
-    margin-top: 50px;
-    margin-bottom: 50px;
-    margin-left: 15px;
+.timer-panel {
+  width: 320px;
+  margin-top: 50px;
+  margin-right: 15px;
+}
 
-    .form-control{
-      height: 100%;
-      text-align: left;
-      overflow: auto;
-      white-space: pre-wrap;
-    }
+.note {
+  flex-grow: 1;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  margin-left: 15px;
 
-    .copyright {
-      text-align: right;
-      color: #CCC;
-      padding-top: 15px;
-      padding-bottom: 5px;
-      margin-bottom: 0;
-
-      a{
-        color: #AAA;
-      }
+  .form-control {
+    height: 100%;
+    text-align: left;
+    overflow: auto;
+    white-space: pre-wrap;
+    line-height: 1rem;
+    &#textarea{
+      line-height: 1.5rem;
     }
   }
 
-  .card {
-    margin-top: 0.5em;
-    border: 1px solid #CCC;
-  }
+  .copyright {
+    text-align: right;
+    color: #CCC;
+    padding-top: 15px;
+    padding-bottom: 5px;
+    margin-bottom: 0;
 
-  .alert {
-    position: fixed;
-    top: 10px;
-    right: 50px;
-    z-index: 5;
+    a {
+      color: #AAA;
+    }
+  }
+}
+
+.card {
+  margin-top: 0.5em;
+  border: 1px solid #CCC;
+}
+
+.alert {
+  position: fixed;
+  top: 10px;
+  right: 50px;
+  z-index: 5;
+  display: none;
+}
+
+.show-task-panel {
+  margin-bottom: 1em;
+}
+
+#log {
+  list-style: none;
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+input::placeholder {
+  color: #D8D8D8;
+}
+
+.fade-enter-active,
+.fade-leave,
+.fade-enter-to {
+  transition: all 1s;
+  opacity: 1;
+  background-color: red;
+}
+
+.fade-enter,
+.fade-leave-active,
+.fade-leave-to {
+  opacity: 0.3;
+  transition: all 1s;
+  background-color: yellow;
+}
+
+
+@media screen and (max-width: 480px) {
+  .note {
     display: none;
   }
-
-  .show-task-panel {
-    margin-bottom: 1em;
-  }
-
-  #log {
-    list-style: none;
-    margin-top: 0;
-    margin-bottom: 0;
-  }
-
-  input::placeholder {
-    color: #D8D8D8;
-  }
-
-  .fade-enter-active,
-  .fade-leave,
-  .fade-enter-to {
-    transition: all 1s;
-    opacity: 1;
-    background-color: red;
-  }
-  .fade-enter,
-  .fade-leave-active,
-  .fade-leave-to {
-    opacity: 0.3;
-    transition: all 1s;
-    background-color: yellow;
-  }
-
-
-  @media screen and (max-width: 480px) {
-    .note{
-      display: none;
-    }
-  }
+}
 </style>
